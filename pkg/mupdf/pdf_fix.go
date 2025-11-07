ab7a9842050a804f9535e9888b6cba8eea204960
Joseph R. Quinn <423821+quinnjr@users.noreply.github.com>
2025-11-07 13:50:40 -0500

    fix: remove extract library from remaining CGO files
    Complete the removal of -lextract from all CGO linker flags
    since we don't use DOCX output functionality.

--- pkg/mupdf/pdf_fix.go
+++ pkg/mupdf/pdf_fix.go
@@ -16,7 +16,7 @@ package mupdf
 
 /*
 #cgo CFLAGS: -I/usr/include
-#cgo LDFLAGS: -lmupdf -lfreetype -ljpeg -lpng -lz -ljbig2dec -lopenjp2 -lharfbuzz -lgumbo -lextract -lmujs -lm
+#cgo LDFLAGS: -lmupdf -lfreetype -ljpeg -lpng -lz -ljbig2dec -lopenjp2 -lharfbuzz -lgumbo -lmujs -lm
 
 #include <stdlib.h>
 #include <string.h>
